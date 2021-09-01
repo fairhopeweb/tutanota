94dc3f544881c7ef1a83410f7d38ec48e8b4c3ba
jom <jom@tutao.de>
2021-09-01 12:01:46 +0200

    Drag and drop for calendar events
    This commit also includes a large refactoring of calendar day/week view
    They had so much code duplicated that it made sense for them to share their implementation
    
    There are still differences that could be extracted at some point

--- src/mail/editor/SendMailModel.js
+++ src/mail/editor/SendMailModel.js
@@ -774,7 +774,7 @@ export class SendMailModel {
 		}
 
 		// The next check depends on contacts being available
-		await this._waitForResolvedRecipients()
+		await this.waitForResolvedRecipients()
 
 		// No password in external confidential mail is an error
 		if (this.isConfidentialExternal()
@@ -976,7 +976,7 @@ export class SendMailModel {
 	/**
 	 * Makes sure the recipient type and contact are resolved.
 	 */
-	_waitForResolvedRecipients(): Promise<RecipientInfo[]> {
+	waitForResolvedRecipients(): Promise<RecipientInfo[]> {
 		return Promise.all(this.allRecipients().map(recipientInfo => {
 			return resolveRecipientInfo(this._worker, recipientInfo).then(recipientInfo => {
 				if (recipientInfo.resolveContactPromise) {
